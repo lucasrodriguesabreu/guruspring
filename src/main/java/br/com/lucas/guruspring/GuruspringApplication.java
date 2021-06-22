635ace49bb0b11ddf8b98a12dc23665928ff4fa2
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2021-06-22 10:27:44 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/lucas/guruspring/GuruspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.guruspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruspringApplication.class, args);
+    }
+
+}
